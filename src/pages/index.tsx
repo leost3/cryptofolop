cdd5f18b945aca8f03177a318003a132da2ef5a6
leonardo studart <lstudart@pyx4.com>
2020-12-23 21:09:33 -0500

    fetch coingeck api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,27 @@
 import { Main } from 'components/Main'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import React from 'react'
+import { Coin } from 'types'
 
-export default function Home() {
-  return <Main />
+export default function Home({
+  coins
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <Main {...coins} />
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  //TODO: import this url from process.env
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false'
+  const res = await fetch(url)
+  const coins: Coin[] = await res.json()
+
+  return {
+    props: {
+      coins: {
+        coins
+      }
+    }
+  }
 }
