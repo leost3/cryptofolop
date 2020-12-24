183fb4aeef3d463869a2bfa4dc94a7939c762de8
leonardo studart <lstudart@pyx4.com>
2020-12-23 21:08:23 -0500

    create icons_path util

---
+++ utils/iconsPath.ts
@@ -0,0 +1,27 @@
+export const increase = '/img/income.svg'
+export const decrease = '/img/decrease.svg'
+
+export const bitcoin_empty = '/img/bitcoin_empty.svg'
+export const bitcoin_filled = '/img/bitcoin_filled.svg'
+
+export const bitcoin_logo = '/img/bitcoin_logo.svg'
+
+export const bitcoin = '/img/bitcoin.svg'
+
+export const dollar = '/img/dollar.svg'
+
+export const grid = '/img/grid.svg'
+
+export const laptop = '/img/laptop.svg'
+
+export const logo = '/img/logo.svg'
+
+export const money = '/img/money.svg'
+
+export const moon = '/img/moon.svg'
+
+export const searching = '/img/searching.svg'
+
+export const sun = '/img/sun.svg'
+
+export const task = '/img/task.svg'
