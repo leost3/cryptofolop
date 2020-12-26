c31147898aab270998a157a87de8c9585a226525
leonardo studart <lstudart@pyx4.com>
2020-12-25 21:39:35 -0500

    add tawilnd preprocessor to apply directive

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,10 +18,6 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
 `
 
 export default GlobalStyles
